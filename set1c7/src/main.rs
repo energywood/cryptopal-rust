7b5d7d1619b68cd586a8725fdfb89abbf549dcb5
gzhong <gzhong.git@outlook.com>
2019-06-05 15:22:06 -0400

    set1c7 done

--- set1c7/src/main.rs
+++ set1c7/src/main.rs
@@ -1,18 +1,50 @@
 extern crate crypto;
 extern crate rand;
 
+use std::env;
+use std::fs;
 use crypto::{ symmetriccipher, buffer, aes, blockmodes, ed25519 };
 use crypto::buffer::{ ReadBuffer, WriteBuffer, BufferResult };
 
 use rand::{ Rng, OsRng };
 
+fn decrypt(encrypted_data: &[u8], key: &[u8]) -> Result<Vec<u8>, symmetriccipher::SymmetricCipherError> {
+    let mut decryptor = aes::ecb_decryptor(
+            aes::KeySize::KeySize128,
+            key,
+            blockmodes::NoPadding);
+
+    let mut final_result = Vec::<u8>::new();
+    let mut read_buffer = buffer::RefReadBuffer::new(encrypted_data);
+    let mut buffer = [0; 4096];
+    let mut write_buffer = buffer::RefWriteBuffer::new(&mut buffer);
+
+    loop {
+        let result = decryptor.decrypt(&mut read_buffer, &mut write_buffer, true)?;
+        final_result.extend(write_buffer.take_read_buffer().take_remaining().iter().map(|&i| i));
+        match result {
+            BufferResult::BufferUnderflow => break,
+            BufferResult::BufferOverflow => { }
+        }
+    }
+
+    Ok(final_result)
+}
 
 fn main() {
-    let mut rng = OsRng::new().unwrap();
-    let mut seed:[u8;32] = [0;32];
-    rng.fill_bytes(&mut seed);
-    let (sk, pk) = ed25519::keypair(&seed);
-    let sig = ed25519::signature(b"Hello world", &sk);
-    let res = ed25519::verify(b"Hello world", &pk, &sig);
-    println!("Hello, world! {}", res);
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
+    println!("In file {}", filename);
+
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+    let mut spec = data_encoding::BASE64.specification();
+    spec.ignore.push_str("\n");
+    let base64 = spec.encoding().unwrap();
+    let content_b = base64.decode(contents.as_bytes()).expect("invalid b64");
+
+
+    let key = b"YELLOW SUBMARINE";
+    let result = decrypt(&content_b, key).expect("cannot decrypt");
+    let plaintext = String::from_utf8(result);
+    println!("{}", plaintext.unwrap());
 }
