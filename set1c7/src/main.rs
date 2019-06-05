5f3da60c8e83861322ef90c5a274e1353e898a2e
gzhong <gzhong.git@outlook.com>
2019-06-05 14:39:37 -0400

    set1c7 wip

--- set1c7/src/main.rs
+++ set1c7/src/main.rs
@@ -1,3 +1,18 @@
+extern crate crypto;
+extern crate rand;
+
+use crypto::{ symmetriccipher, buffer, aes, blockmodes, ed25519 };
+use crypto::buffer::{ ReadBuffer, WriteBuffer, BufferResult };
+
+use rand::{ Rng, OsRng };
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut rng = OsRng::new().unwrap();
+    let mut seed:[u8;32] = [0;32];
+    rng.fill_bytes(&mut seed);
+    let (sk, pk) = ed25519::keypair(&seed);
+    let sig = ed25519::signature(b"Hello world", &sk);
+    let res = ed25519::verify(b"Hello world", &pk, &sig);
+    println!("Hello, world! {}", res);
 }
